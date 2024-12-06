a79987cb12cec287ce0a497f1c063718e612e7a2
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-06 20:06:38 +0600

    complete

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,6 @@ let button = document.getElementById("button");
 let qrImg = document.getElementById("qr-img");
 let h = document.getElementById("h");
 // Function
-
 function qrGenerate() {
   qrImg.src =
     "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
